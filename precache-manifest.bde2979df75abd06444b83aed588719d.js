6b95f1b5025b92b2a0f0c6a253174b9d4721a0be
Jubo-cade <jubomdivnishvili@gmail.com>
2020-10-08 14:43:05 +0400

    Updates

---
+++ precache-manifest.bde2979df75abd06444b83aed588719d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eabf3e425d6329975e313cf0dce7c10",
+    "url": "/recipes-app/index.html"
+  },
+  {
+    "revision": "d84f44ceb22b27da58bb",
+    "url": "/recipes-app/static/css/main.70429c2f.chunk.css"
+  },
+  {
+    "revision": "1be547f21408250b4ed6",
+    "url": "/recipes-app/static/js/2.dbf748bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes-app/static/js/2.dbf748bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d84f44ceb22b27da58bb",
+    "url": "/recipes-app/static/js/main.3052434a.chunk.js"
+  },
+  {
+    "revision": "7b8819e78c07f8dc2db0",
+    "url": "/recipes-app/static/js/runtime-main.a3d12a88.js"
+  }
+]);
